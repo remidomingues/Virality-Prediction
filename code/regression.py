0e98269f4a030d5dbcd5794f95cb5f56d1748ea6
Rémi Domingues <remidomingues@live.fr>
2015-05-03 18:36:15 +0200

    Refactored regression model

--- code/regression.py
+++ code/regression.py
@@ -1,63 +1,73 @@
 from sklearn import linear_model
 import numpy as np
 import h5py
+import pickle
 
 class RegressionModel:
     # HDFS file path
     HDFS_FILEPATH = "../data/output.hdf5"
     # Traing set proportion
     TRAINING_SIZE = 0.8
+    # Serialization file
+    SERIALIZATION_FILE = "../data/regression_model"
 
-    def __init__(self):
-        self.load_dataset()
-
-    def loadDataFromHDFS(self):
+    @staticmethod
+    def __loadDataFromHDFS():
         """
         Load data from the specified HDFS file. Structure is Dataset => Attributes
         IDs => ID
         Features => [followers_count, friends_count, listed_count, statuses_count,
                      hashtags_count, media_count, user_mention_count, url_count,
-                     verified_account, is_a_retweet]
+                     verified_account, is_a_retweet, tweet_length]
         Virality => [retweet_count, favorite_count, combined_count]
         """
         print "Importing data..."
-        fileObj = h5py.File(self.HDFS_FILEPATH, 'r')
+        fileObj = h5py.File(RegressionModel.HDFS_FILEPATH, 'r')
         idList = fileObj["IDs"]
         features = fileObj["Features"]
-        self.feature_size = len(features[0])
         virality = fileObj["Virality"]
-        self.virality_size = len(virality[0])
         return [idList, features, virality]
 
-    def load_dataset(self):
+    @staticmethod
+    def load_datasets():
+        """
+        Return the training and testing datasets containing
+        the tweets featured followed by the retweet count
+        """
         # Import data
-        hdfs_data = self.loadDataFromHDFS()
-        # Concatenate the three arrays into one along the second axis
-        data = np.c_[hdfs_data[0], hdfs_data[1], hdfs_data[2]]
+        hdfs_data = RegressionModel.__loadDataFromHDFS()
+        # Concatenate the arrays into one along the second axis
+        data = np.c_[hdfs_data[1], hdfs_data[2][:, 0]]
         # Shuffle data
         np.random.shuffle(data)
         # Split dataset into training and testing sets
-        size = int(len(data) * self.TRAINING_SIZE)
-        self.training_set = data[:size]
-        self.testing_set = data[len(data)-size:]
+        size = int(len(data) * RegressionModel.TRAINING_SIZE)
+        training_set = data[:size]
+        testing_set = data[len(data)-size:]
+        return [training_set, testing_set]
 
-    def train(self):
+    def train(self, training_set):
+        """
+        Train the classifier
+        """
         print "Training model..."
         # Features
-        X_train = self.training_set[:, 1:self.feature_size+1]
+
+        X_train = training_set[:, :-1]
         # Retweet count
-        Y_train = self.training_set[:, self.feature_size+1]
+        Y_train = training_set[:, -1]
         # Model training
         self.clf = linear_model.BayesianRidge()
         self.clf.fit(X_train, Y_train)
-        # Compute model efficiency
-        self.score()
 
-    def score(self):
+    def score(self, testing_set):
+        """
+        Compute benchmarks according to the testing dataset
+        """
         # Features
-        X_test = self.testing_set[:, 1:11]
+        X_test = testing_set[:, :-1]
         # Retweet count
-        Y_test = self.testing_set[:, 11]
+        Y_test = testing_set[:, -1]
         # Model coefficients
         print "Coefficients: ", self.clf.coef_
         # Mean squared error
@@ -74,8 +84,8 @@ class RegressionModel:
 
 
 if __name__ == "__main__":
+    [training_set, testing_set] = RegressionModel.load_datasets()
     model = RegressionModel()
-    model.train()
-    features = [[950, 1585, 34, 3173, 0, 0, 0, 2, 0, 0],
-                [23988, 1770, 164, 123556, 1, 0, 0, 1, 0, 0]]
-    print "Prediction samples: ", model.predict(features)
+    model.train(training_set)
+    model.score(testing_set)
+    print "Prediction samples: ", model.predict([testing_set[0][:-1], testing_set[1][:-1]])
